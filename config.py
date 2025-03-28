40a2829b18670e5ccb30399c11a85eb3dc40b054
SMTD <o.hamid@smtchgz.cn>
2025-03-29 00:21:30 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 import os
+from dotenv import load_dotenv  # نستورد هذه المكتبة
+
+# تحميل متغيرات البيئة من ملف .env
+load_dotenv()
 
 class Config:
-    BOT_TOKEN = os.getenv("8029361280:AAE5tcESXsx7TVb5pLnQWnK0bKfhgeT1eFA")
-    ADMIN_IDS=1819080408,6547641571
+    BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")  # سيقرأ القيمة من ملف .env
+    ADMIN_IDS = [1819080408, 6547641571]  # أصبحت الآن قائمة (list) بدلًا من tuple
